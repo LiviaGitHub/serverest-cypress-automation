bce875cb35f2bdbe8c81cfe42274b2af4dd21520
Livia Bonifacio <liviagithub@gmail.com>
2026-08-19 12:44:11 +0400

    chore: initialize Cypress project

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    baseUrl: 'https://front.serverest.dev',
+    env: { apiUrl: 'https://serverest.dev' },
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
+    },
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    video: true,
+    retries: { runMode: 2, openMode: 0 }
+  }
+});
